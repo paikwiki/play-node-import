b6499bfa094775a768fcd7974ae346a4541d001e
paikwiki <paikwiki@gmail.com>
2024-07-07 22:35:00 +0900

    :adhesive_bandage: Rename variable

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 (() => {
-    const aliasImport = fs_1.default.readFileSync("./dist/index-namespace-import.js", "utf8");
+    const namespaceImport = fs_1.default.readFileSync("./dist/index-namespace-import.js", "utf8");
     const namedImport = fs_1.default.readFileSync("./dist/index-named-import.js", "utf8");
-    console.log(`./dist/index-namespace-import.js\t${aliasImport.length}`);
+    console.log(`./dist/index-namespace-import.js\t${namespaceImport.length}`);
     console.log(`./dist/index-named-import.js\t\t${namedImport.length}`);
 })();
